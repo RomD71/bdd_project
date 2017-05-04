97af3be9938ca343eea042c31ffbfb42b0db869a
Maxime <maxime.clerc71@gmail.com>
2017-05-04 11:28:21 +0200

    commit affichage article

--- src/OC/projetBddBundle/Controller/HomeController.php
+++ src/OC/projetBddBundle/Controller/HomeController.php
@@ -3,11 +3,23 @@
 namespace OC\projetBddBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BddBundle:Home:index.html.twig');
+        $repos = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('BddBundle:Article')
+            ->findAll();
+
+        $content = $this->get('templating')->render('BddBundle:Home:index.html.twig',[
+            'article' => $repos,
+        ]);
+        return new Response($content);
+
+//        return $this->render('BddBundle:Home:index.html.twig');
     }
 }
