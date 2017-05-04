b8fc74d61d58e409a4c47fe89f43bfdeda43764f
Maxime <maxime.clerc71@gmail.com>
2017-05-04 10:48:17 +0200

    commit controller/vue ok

--- src/OC/projetBddBundle/Controller/HomeController.php
+++ src/OC/projetBddBundle/Controller/HomeController.php
@@ -4,7 +4,7 @@ namespace OC\projetBddBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class HomeController extends Controller
 {
     public function indexAction()
     {
