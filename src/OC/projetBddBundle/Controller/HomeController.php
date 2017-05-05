d861767d8a150819225a86225ed944a0f495211a
Maxime <maxime.clerc71@gmail.com>
2017-05-05 10:14:08 +0200

    commit ajout article

--- src/OC/projetBddBundle/Controller/HomeController.php
+++ src/OC/projetBddBundle/Controller/HomeController.php
@@ -2,22 +2,43 @@
 
 namespace OC\projetBddBundle\Controller;
 
+use DateTime;
+use OC\projetBddBundle\Entity\Article;
+use OC\projetBddBundle\Form\ArticleType;
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $article = new Article();
+        $formArticle = $this->createForm(ArticleType::class, $article);
+
         $repos = $this
             ->getDoctrine()
             ->getManager()
             ->getRepository('BddBundle:Article')
             ->findAll();
 
-        dump($repos);
+        $formArticle->handleRequest($request);
+        if ($formArticle->isSubmitted() && $formArticle->isValid()) {
+
+            $date = new DateTime();
+            $em = $this->getDoctrine()->getManager();
+            $article->setDatePublication($date);
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('bdd_homepage');
+        }
+
         $content = $this->get('templating')->render('BddBundle:Home:index.html.twig', [
             'article' => $repos,
+            'formArticle' => $formArticle->createView(),
         ]);
         return new Response($content);
     }
---
+++ src/OC/projetBddBundle/Form/ArticleType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace OC\projetBddBundle\Form;
+
+use OC\projetBddBundle\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'label' => "Titre de l'article : "
+            ])
+            ->add('contenuArticle', TextareaType::class, [
+                'label' => "Contenu de l'article : "
+            ])
+            ->add('valider', SubmitType::class);
+//            ->add('idFichier');
+//            ->add('idMotCle');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Article::class
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'oc_projetbddbundle_article';
+    }
+
+
+}
